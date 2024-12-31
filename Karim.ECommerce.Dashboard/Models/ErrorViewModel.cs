d88c565bda0a36153312dd2d3d5e23cdb413627f
Karim Fathy <139636760+So3S03@users.noreply.github.com>
2024-12-31 19:15:59 +0200

    Deleted Unnessary Projects

--- Karim.ECommerce.Dashboard/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Karim.ECommerce.Dashboard.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
